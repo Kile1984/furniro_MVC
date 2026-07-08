fb2017e63ebd425fbc779122814ef3f0468aa6b0
Ilija Zikic <tvojemail@example.com>
2026-07-08 06:40:45 +0200

    Implement loading logo on load page

--- src/js/main.js
+++ src/js/main.js
@@ -3,9 +3,11 @@ import { initEventManager } from "./events/eventManager.js";
 import { loadProducts } from "./controllers/homeController.js";
 import { syncHeaderCounts } from "./controllers/headerController.js";
 import { renderCompareTray } from "./controllers/compareTrayController.js";
+import { hideLoader } from "./views/loaderView.js";
 
 const init = async function () {
   await loadProducts();
+  hideLoader();
   initRouter();
   initEventManager();
   syncHeaderCounts();
---
+++ src/js/views/loaderView.js
@@ -0,0 +1,5 @@
+const loader = document.querySelector(".loading-overlay");
+
+export const hideLoader = function () {
+  loader.classList.add("hidden");
+};
