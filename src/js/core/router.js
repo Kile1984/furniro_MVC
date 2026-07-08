0fbd232122bf993b08493167c4f59c2cee3717f2
Ilija Zikic <zikicilija@gmail.com>
2026-07-08 15:01:33 +0200

    Refactor router structure

--- src/js/core/router.js
+++ src/js/core/router.js
@@ -1,7 +1,37 @@
-import { controlRoutes } from "../controllers/routerController.js";
+import { routerActions } from "../state/actions/routerActions.js";
+import { renderApp } from "../core/render.js";
 
 export const initRouter = function () {
-  window.addEventListener("hashchange", controlRoutes);
+  window.addEventListener("hashchange", renderCurrentRoute);
 
-  controlRoutes();
+  renderCurrentRoute();
 };
+
+function renderCurrentRoute() {
+  document.documentElement.style.scrollBehavior = "auto";
+  document.documentElement.scrollTop = 0;
+  document.documentElement.style.scrollBehavior = "";
+
+  const hash = window.location.hash.slice(1) || "/";
+  const route = hash === "/" ? "/" : "/" + hash.split("/")[1];
+
+  const routesMap = {
+    "/": "home",
+    "/shop": "shop",
+    "/about": "about",
+    "/contact": "contact",
+    "/payment": "payment",
+    "/returns": "returns",
+    "/policy": "policy",
+    "/cart": "cart",
+    "/wishlist": "wishlist",
+    "/product": "product",
+    "/category": "category",
+  };
+
+  const view = routesMap[route] || "home";
+
+  routerActions.setRoute(view);
+
+  renderApp();
+}
